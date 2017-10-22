4c65ed6ffb16dfbc9e3b8183aa11d20bc04dca8f
Jeason Centaure <jcentaur@student.42.fr>
2017-10-22 20:45:12 +0200

    pthread

--- ft_rt.c
+++ ft_rt.c
@@ -6,7 +6,7 @@
 /*   By: jcentaur <jcentaur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/21 02:23:38 by jcentaur          #+#    #+#             */
-/*   Updated: 2017/10/21 02:23:58 by jcentaur         ###   ########.fr       */
+/*   Updated: 2017/10/22 18:27:32 by jcentaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,32 +95,88 @@ void 		ft_refrac(t_scene *scene, t_ray *ray)
 		ft_light(scene, ray);
 }
 
-void		ft_scene(t_sdl *sdl, t_scene *scene)
+void		*ft_thread(void  *data)
 {
-	double	x;
-	double	y;
-	t_ray	ray;
+	t_sdlpp	*s;
 	t_cam	c;
+	t_ray	ray;
+	int		x;
+	int		xx;
+	int		y;
+	int		limx;
+	int		limy;
 
-	ft_cal_vec_cam(scene, &c);
-	ray.eye = scene->cam.pos;
-	y = 0;
-	while (y < H / scene->res)
+	s = (t_sdlpp *)data;
+	pthread_mutex_lock(&(s->lock));
+	ft_cal_vec_cam(s->scene, &c);
+	ray.eye = s->scene->cam.pos;
+	y = s->y;
+	xx = s->x;
+	limx= s->limx;
+	limy= s->limy;
+	pthread_cond_signal(&(s->cond));
+	pthread_mutex_unlock(&(s->lock));
+	while (y < limy)
 	{
-		x = 0;
-		while (x < L / scene->res)
+		x = xx;
+		while (x < limx)
 		{
-			ray.dir = ft_camera(scene, c, x, y);
+			pthread_mutex_lock(&(s->lock));
+			ray.dir = ft_camera(s->scene, c, x, y);
 			ft_normal(&ray.dir);
-			sdl->pixels[(int)(y * (L) + x)] = rgb(filter(ft_vect(0, 0, 0), scene->filter));
-			if (ft_inter_obj(scene, &ray) != 0)
+			s->sdl->pixels[(int)(y * L + x)] = rgb(filter(ft_vect(0, 0, 0), s->scene->filter));
+			if (ft_inter_obj(s->scene, &ray) != 0)
 			{
-				ft_reflec(scene, &ray, 0);
-				ft_refrac(scene, &ray);
-				sdl->pixels[(int)(y * L + x)] = rgb(filter(ray.color, scene->filter));
+				ft_reflec(s->scene, &ray, 0);
+				ft_refrac(s->scene, &ray);
+				s->sdl->pixels[(int)(y * L + x)] = rgb(filter(ray.color, s->scene->filter));
 			}
+			pthread_mutex_unlock(&(s->lock));
 			x++;
 		}
 		y++;
 	}
+	// printf("x = %d\ny = %d\nlimx = %d\nlimy = %d\n\n", s->x, s->y, s->limx, s->limy);
+	pthread_exit(NULL);
+}
+
+void		ft_scene(t_sdl *sdl, t_scene *scene)
+{
+	t_sdlpp	*s;
+	pthread_t	thread[4];
+
+	s = (t_sdlpp *)malloc(sizeof(t_sdlpp));
+	s->lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+	s->cond = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+	s->y = 0;
+	s->x = 0;
+	s->limx = L / (scene->res * 2.0);
+	s->limy = H / (scene->res * 2.0);
+	s->sdl = sdl;
+	s->scene = scene;
+	pthread_create(&thread[0],  NULL,ft_thread, (void *)s);
+	pthread_mutex_lock(&(s->lock));
+	pthread_cond_wait(&(s->cond), &(s->lock));
+	s->x += s->limx;
+	s->limx += s->limx;
+	pthread_mutex_unlock(&(s->lock));
+	pthread_create(&thread[1],  NULL,ft_thread, (void *)s);
+	pthread_mutex_lock(&(s->lock));
+	pthread_cond_wait(&(s->cond), &(s->lock));
+	s->x = 0;
+	s->limx = L / (scene->res * 2.0);
+	s->y += s->limy;
+	s->limy += s->limy;
+	pthread_mutex_unlock(&(s->lock));
+	pthread_create(&thread[2],  NULL,ft_thread, (void *)s);
+	pthread_mutex_lock(&(s->lock));
+	pthread_cond_wait(&(s->cond), &(s->lock));
+	s->x += s->limx;
+	s->limx += s->limx;
+	pthread_mutex_unlock(&(s->lock));
+	pthread_create(&thread[3],  NULL,ft_thread, (void *)s);
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
+	pthread_join(thread[2], NULL);
+	pthread_join(thread[3], NULL);
 }
